2867ff130bc425545bde67511283944fe3bfbe8b
Deshmukh P <deshmukhpurushothaman@gmail.com>
2022-08-11 11:04:54 +0530

    🔐 Authentication added

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './current-user.decorator';
+import JwtAuthGuard from './guards/jwt-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { User } from './users/schemas/user.schema';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  async login(
+    @CurrentUser() user: User,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    await this.authService.login(user, response);
+    response.send(user);
   }
+
 }
